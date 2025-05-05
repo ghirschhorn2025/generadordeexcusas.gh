d878b870f08582f496fa7a4fbda30a2437eac77a
Gerardo Hirschhorn <ghirschhorn@outlook.com>
2025-05-05 12:26:29 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ let when = ['ayer', 'hoy por la mañana', 'anoche', 'ayer por la tarde'];
+ let who = ['mi perro', 'el gato de mi vecino', 'mi sobrina', 'mi pareja', 'un alien'];
+ let action = ['se comió', 'defecó sobre', 'olvidó', 'no ha impreso', 'se llevó'];
+ let what = ['mi tarea', 'mis documentos', 'la tarjeta de embarque', 'los apuntes para estudiar'];
+
+
+ function generarExcusa () {
+   let randomWhen = when[Math.floor(Math.random() * when.length)];
+   let randomWho = who[Math.floor(Math.random() * who.length)];
+   let randomAction = action[Math.floor(Math.random() * action.length)];
+   let randomWhat = what[Math.floor(Math.random() * what.length)];
+    return `${randomWhen} ${randomWho} ${randomAction} ${randomWhat}.`;
+ }
+
+
+ let excusa = document.getElementById("genExcuse");
+ excusa.innerText = generarExcusa();
+
+
+ let boton = document.getElementById("generate");
+ boton.addEventListener("click", function () {
+   excusa.innerText = generarExcusa();
+ });
+
+
 };
